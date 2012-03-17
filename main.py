eb819f1622bae2677cb1ea913bc777229c6a3eba
Natan Zabkar <natan.zabkar@gmail.com>
2012-03-17 23:19:02 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+
+"""
+
+* File Name : main.py
+
+* Created By : Natan Žabkar, natan.zabkar@gmail.com 
+
+* Creation Date : 17-03-2012
+
+* Last Modified : 17.3.2012 23:18:42
+
+"""
+
+
