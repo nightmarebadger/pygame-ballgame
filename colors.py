8fdbbfc384e6ec8ddc4a8b1754dd0f2827e5efca
Natan Zabkar <natan.zabkar@gmail.com>
2012-03-17 23:24:18 +0100

    Added a colors module and imports

---
+++ colors.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+
+"""
+
+* File Name : colors.py
+
+* Created By : Natan Žabkar, natan.zabkar@gmail.com 
+
+* Creation Date : 17-03-2012
+
+* Last Modified : 17.3.2012 23:20:28
+
+"""
+
+# Colors module, so I don't have to write it all down everytime :)
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
--- main.py
+++ main.py
@@ -8,8 +8,10 @@
 
 * Creation Date : 17-03-2012
 
-* Last Modified : 17.3.2012 23:18:42
+* Last Modified : 17.3.2012 23:23:54
 
 """
 
-
+import pygame, random, sys
+from pygame.locals import *
+from colors import *
